f0428261c5eeec51b8ab7fd116333f2a75290881
Amos Tan <alphatrl1997@gmail.com>
2017-01-03 22:58:39 +0800

    Reduce code size

--- Pickup.h
+++ Pickup.h
@@ -59,11 +59,16 @@ public:
 	PickupType getPickupType() { return this->type; }			// returns the type of pickup (i.e. blackhole, health, stun, freeze)
 	bool getIsDestructor() { return this->isDestructor; }		// returns true for destructor, false for obstructor
 	void calculateObstructorDestructorType();					// calculate the type of pickup
+	void setNewLocation();										// set new x and y location to Pickup
+	void respawnPickup();										// respawn pickup after it is collected
+	int minMaxRand_Pickup(int min, int max);									// generate a random integer based on minimum/maximum value
+
 
 private:
 
 	PickupType type;
 	boolean		isDestructor;
+
 };
 
 #endif
